e8e59ffd648fdb66c57c228db54b47dd5fab06c6
cygap <113382267+Cygap@users.noreply.github.com>
2023-03-22 13:16:08 +0100

    added clustering - need to check the linrary, sometimes does not renders properly on Node 18

--- src/components/MarkersLayer.tsx
+++ src/components/MarkersLayer.tsx
@@ -1,43 +1,37 @@
 import { useContext } from "react";
-import { MapContext } from "../providers/MapContext";
-import { Marker } from "@react-google-maps/api";
-import CONSTRUCTION from "../assets/construction-roller.svg";
-import ROAD_CONDITION from "../assets/blocker.svg";
-import SPECIAL_EVENT from "../assets/ferris-wheel.svg";
-import WEATHER_CONDITION from "../assets/weather.svg";
-import INCIDENT from "../assets/sports-car-crash.svg";
+import { eventType, MapContext } from "../providers/MapContext";
+import { Marker, MarkerClusterer } from "@react-google-maps/api";
+
 import { TrafficEvent } from "../providers/types";
 
-export const eventType = {
-  CONSTRUCTION,
-  SPECIAL_EVENT,
-  WEATHER_CONDITION,
-  ROAD_CONDITION,
-  INCIDENT
-};
 export default function MarkersLayer() {
   const state = useContext(MapContext);
   const markers: TrafficEvent[] = state ? state.getFilteredMarkers() || [] : [];
+  const options = {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    minimumClusterSize: 3,
+    maxZoom: 13
+  };
 
   return (
-    <>
-      {markers.length ? (
-        markers.map((marker) => {
-          return (
+    <MarkerClusterer options={options}>
+      {(clusterer) => (
+        <>
+          {markers.map((marker) => (
             <Marker
+              clusterer={clusterer}
               position={{
                 lat: marker.geography.coordinates[1],
                 lng: marker.geography.coordinates[0]
               }}
-              key={marker.id}
               title={marker.event_type}
               icon={eventType[marker.event_type]}
+              key={marker.id}
             />
-          );
-        })
-      ) : (
-        <></>
+          ))}
+        </>
       )}
-    </>
+    </MarkerClusterer>
   );
 }
